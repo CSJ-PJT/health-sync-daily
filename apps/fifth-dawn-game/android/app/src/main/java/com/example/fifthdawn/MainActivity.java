55be0b818433c55c4c4107176b7d8cc5c44bd543
CSJ <dan1846@naver.com>
2026-04-09 11:41:30 +0900

    fix(game): add native fifth dawn debug overlay

--- apps/fifth-dawn-game/android/app/src/main/java/com/example/fifthdawn/MainActivity.java
+++ apps/fifth-dawn-game/android/app/src/main/java/com/example/fifthdawn/MainActivity.java
@@ -1,5 +1,30 @@
 package com.example.fifthdawn;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView.setWebContentsDebuggingEnabled(true);
+
+        WebView webView = findViewById(com.getcapacitor.android.R.id.webview);
+        if (webView == null) {
+            webView = findViewById(getResources().getIdentifier("webview", "id", getPackageName()));
+        }
+
+        if (webView != null) {
+            webView.setBackgroundColor(Color.parseColor("#07111F"));
+        }
+
+        View overlay = findViewById(getResources().getIdentifier("native_debug_overlay", "id", getPackageName()));
+        if (overlay != null) {
+            overlay.bringToFront();
+        }
+    }
+}
